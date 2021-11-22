97a0cd3566cb53dd6cbc5d1007ff305114f11ecc
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2021-11-22 13:42:36 +0400

    Add files via upload

---
+++ 3zanyatie(dolg)/3_1seminar.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void s_srav(double r, double a);
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double r, a;
+	cin >> r >> a;
+	s_srav(r, a);
+}
+void s_srav(double r, double a)
+{
+	double pi = 4 * atan(1);
+	if (r > 0 && a > 0)
+	{
+		if (pi * r * r < a * a)
+			cout << "S круга меньше S квадрата" << endl;
+		if (pi * r * r > a * a)
+			cout << "S квадрата меньше S круга" << endl;
+		else cout << "S равны" << endl;
+	}
+	else cout << "Данные некорректны" << endl;
+}
---
+++ 3zanyatie(dolg)/3_2seminar.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+bool s_bool(double a);
+void s_sqrt(double a);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c;
+	cin >> a >> b >> c;
+	s_sqrt(a); s_sqrt(b); s_sqrt(c);
+}
+
+
+
+bool s_bool(double a)
+{
+	if (a >= 0)
+		return true;
+	else return false;
+}
+void s_sqrt(double a)
+{
+	if (s_bool(a) == true)
+		cout << fixed << setprecision(4) << sqrt(a) << ' ';
+	else cout << a << ' ';
+}
