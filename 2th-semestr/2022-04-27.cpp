6a9680c01716b1969ce764adf53d1d864e5f604d
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-04-27 01:56:30 +0400

    Update 2022-04-27.cpp

--- 2th-semestr/2022-04-27.cpp
+++ 2th-semestr/2022-04-27.cpp
@@ -40,3 +40,54 @@ int main() {
     }
 }
 
+//---------------------------(4)--------------------------------------
+//IAF - Insert After First
+void IAF_Stack(stack<int>& Stack) {
+    stack<int> temp;
+    int min = Stack.top();
+    while (!Stack.empty()) {
+        if (Stack.top() < min)
+            min = Stack.top();
+        temp.push(Stack.top());
+        Stack.pop();
+    }
+    Stack.push(temp.top());
+    temp.pop();
+    Stack.push(min);
+    while (!temp.empty()) {
+        Stack.push(temp.top());
+        temp.pop();
+    }
+    while (!Stack.empty()) {
+        temp.push(Stack.top());
+        Stack.pop();
+    }
+    Stack = temp;
+}
+void IAF_Queue(queue<int>& Queue) {
+    int min = Queue.back();
+    for (int i = 0, size = Queue.size(); i < size; i++) {
+        if (Queue.front() < min)
+            min = Queue.front();
+        if (i == 1)
+            Queue.push(min);
+        Queue.push(Queue.front());
+        Queue.pop();
+    }
+}
+void IAF_Vector(vector<int>& Vector) {
+    int min = Vector[0];
+    vector<int>::iterator iter = Vector.begin() + 1;
+    for (int i = 1; i < Vector.size(); i++)
+        if (Vector[i] < min)
+            min = Vector[i];
+    Vector.insert(iter, min);
+}
+void IAF_List(list<int>& List) {
+    int min = List.back();
+    auto iter = List.begin();
+    for (auto i = iter; i != List.end(); i++)
+        if (*i < min)
+            min = *i;
+    List.insert(iter, min);
+}
